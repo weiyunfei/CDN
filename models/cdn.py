74105eee4f762b2a23fe0a5e5b19130bec66841c
Yunfei Wei <weiyf_2333@hotmail.com>
2022-03-26 20:32:58 +0800

    change to multi-branch encoder.

--- models/cdn.py
+++ models/cdn.py
@@ -15,10 +15,13 @@ class CDN(nn.Module):
                  return_intermediate_dec=False):
         super().__init__()
 
-        encoder_layer = TransformerEncoderLayer(d_model, nhead, dim_feedforward,
+        vanilla_encoder_layer = TransformerEncoderLayer(d_model, nhead, dim_feedforward,
+                                                dropout, activation, normalize_before)
+        hoi_encoder_layer = EncoderLayerBottleneck(d_model, nhead, dim_feedforward, 
                                                 dropout, activation, normalize_before)
         encoder_norm = nn.LayerNorm(d_model) if normalize_before else None
-        self.encoder = TransformerEncoder(encoder_layer, num_encoder_layers, encoder_norm)
+
+        self.encoder = EncoderHOI(vanilla_encoder_layer, 4, hoi_encoder_layer, 2, encoder_norm)
 
         decoder_layer = TransformerDecoderLayer(d_model, nhead, dim_feedforward,
                                                 dropout, activation, normalize_before)
@@ -51,8 +54,8 @@ class CDN(nn.Module):
         mask = mask.flatten(1)
 
         tgt = torch.zeros_like(query_embed)
-        memory = self.encoder(src, src_key_padding_mask=mask, pos=pos_embed)
-        hopd_out = self.decoder(tgt, memory, memory_key_padding_mask=mask,
+        memory, memory_inst, memory_verb = self.encoder(src, src_key_padding_mask=mask, pos=pos_embed)
+        hopd_out = self.decoder(tgt, memory_inst, memory_key_padding_mask=mask,
                           pos=pos_embed, query_pos=query_embed)
         hopd_out = hopd_out.transpose(1, 2)
 
@@ -61,7 +64,7 @@ class CDN(nn.Module):
         interaction_query_embed = interaction_query_embed.permute(1, 0, 2)
 
         interaction_tgt = torch.zeros_like(interaction_query_embed)
-        interaction_decoder_out = self.interaction_decoder(interaction_tgt, memory, memory_key_padding_mask=mask,
+        interaction_decoder_out = self.interaction_decoder(interaction_tgt, memory_verb, memory_key_padding_mask=mask,
                                   pos=pos_embed, query_pos=interaction_query_embed)
         interaction_decoder_out = interaction_decoder_out.transpose(1, 2)
 
@@ -90,6 +93,47 @@ class TransformerEncoder(nn.Module):
             output = self.norm(output)
 
         return output
+
+
+class EncoderHOI(nn.Module):
+
+    def __init__(self, vanilla_encoder_layer=TransformerEncoderLayer, num_vanilla_layers=4,
+                 hoi_encoder_layer=EncoderLayerBottleneck, num_hoi_layers=2, normalize_before=False):
+        super().__init__()
+        self.layers = get_clones(vanilla_encoder_layer, num_vanilla_layers)
+        self.hoi_layers = get_clones(hoi_encoder_layer, num_hoi_layers)
+
+        self.norm = self.norm_sub = self.norm_obj = self.norm_verb = None
+        if normalize_before:
+            self.norm = nn.LayerNorm(d_model)
+            self.norm_inst = nn.LayerNorm(d_model)
+            self.norm_verb = nn.LayerNorm(d_model)
+
+    def eye_init(self):
+        for layer in self.hoi_layers:
+            layer.eye_init()
+
+    def forward(self, src,
+                mask: Optional[Tensor] = None,
+                src_key_padding_mask: Optional[Tensor] = None,
+                pos: Optional[Tensor] = None):
+        output = src
+        for layer in self.layers:
+            output = layer(output, src_mask=mask, pos=pos,
+                           src_key_padding_mask=src_key_padding_mask)
+        if self.norm is not None:
+            output = self.norm(output)
+
+        embed_inst = embed_verb = output
+        for layer in self.hoi_layers:
+            embed_inst, embed_verb = layer(
+                embed_inst, embed_verb, src_mask=mask, pos=pos,
+                src_key_padding_mask=src_key_padding_mask)
+        if self.norm_inst is not None:
+            embed_inst = self.norm_inst(embed_inst)
+            embed_verb = self.norm_verb(embed_verb)
+
+        return output, embed_inst, embed_verb
 
 
 class TransformerDecoder(nn.Module):
@@ -274,6 +318,176 @@ class TransformerDecoderLayer(nn.Module):
                                     tgt_key_padding_mask, memory_key_padding_mask, pos, query_pos)
         return self.forward_post(tgt, memory, tgt_mask, memory_mask,
                                  tgt_key_padding_mask, memory_key_padding_mask, pos, query_pos)
+
+
+class EncoderLayerBottleneck(nn.Module):
+
+    def __init__(self,
+                 d_model,
+                 nhead,
+                 dim_feedforward=256,
+                 dropout=0.1,
+                 activation='relu',
+                 normalize_before=False,
+                 **kwargs):
+        super().__init__()
+        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout)
+        self.inst_proj = nn.Linear(d_model, d_model)
+        self.verb_proj = nn.Linear(d_model, d_model)
+
+        self.cross_attn_verb = nn.MultiheadAttention(d_model, nhead, dropout)
+        self.norm_verb = nn.LayerNorm(d_model)
+        self.dropout_verb = nn.Dropout(dropout)
+        # self.reduce_dim = nn.Linear(2 * d_model, d_model)
+
+        self.linear1 = self.dropout = self.linear2 = None
+        self.linear1 = nn.Linear(d_model, dim_feedforward)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, d_model)
+
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.activation = get_activation_fn(activation)
+
+        self.normalize_before = normalize_before
+
+    def with_pos_embed(self, tensor, pos: Optional[Tensor]):
+        return tensor if pos is None else tensor + pos
+
+    def eye_init(self):
+        nn.init.eye_(self.inst_proj.weight)
+        nn.init.constant_(self.inst_proj.bias, 0)
+        nn.init.eye_(self.verb_proj.weight)
+        nn.init.constant_(self.verb_proj.bias, 0)
+
+    def forward_cross_attn(self, src_inst, src_verb, pos, src_key_padding_mask):
+        embed_inst = src_inst
+        embed_obj = src_obj
+
+        # dst_verb = torch.cat([embed_sub, embed_obj], dim=-1)
+        # dst_verb = self.reduce_dim(dst_verb)
+        if self.normalize_before:
+            embed_verb = self.cross_attn_pre(src=src_verb, dst=embed_inst, tgt='verb', pos=pos,
+                                             dst_key_padding_mask=src_key_padding_mask)
+        else:
+            embed_verb = self.cross_attn_post(src=src_verb, dst=embed_inst, tgt='verb', pos=pos,
+                                              dst_key_padding_mask=src_key_padding_mask)
+        return embed_inst, embed_verb
+
+    def cross_attn_post(self, src, dst, tgt,
+                        dst_mask: Optional[Tensor] = None,
+                        dst_key_padding_mask: Optional[Tensor] = None,
+                        pos: Optional[Tensor] = None):
+        src2 = getattr(self, f'cross_attn_{tgt}')(
+            query=self.with_pos_embed(src, pos),
+            key=self.with_pos_embed(dst, pos),
+            value=dst, attn_mask=dst_mask,
+            key_padding_mask=dst_key_padding_mask)[0]
+        src = getattr(self, f'norm_{tgt}')(src + getattr(self, f'dropout_{tgt}')(src2))
+
+        return src
+
+    def cross_attn_pre(self, src, dst, tgt,
+                       dst_mask: Optional[Tensor] = None,
+                       dst_key_padding_mask: Optional[Tensor] = None,
+                       pos: Optional[Tensor] = None):
+        src2 = getattr(self, f'cross_attn_{tgt}')(
+            query=self.with_pos_embed(getattr(self, f'norm_{tgt}')(src), pos),
+            key=self.with_pos_embed(dst, pos),
+            value=dst, attn_mask=dst_mask,
+            key_padding_mask=dst_key_padding_mask)[0]
+        src = src + getattr(self, f'dropout_{tgt}')(src2)
+
+        return src
+
+    def forward_post(self, src_inst, src_verb,
+                     src_mask: Optional[Tensor] = None,
+                     src_key_padding_mask: Optional[Tensor] = None,
+                     pos: Optional[Tensor] = None):
+        src_inst = self.inst_proj(src_inst)
+        q_s = k_s = self.with_pos_embed(src_inst, pos)
+        src_inst2 = self.self_attn(q_s, k_s, value=src_inst, attn_mask=src_mask,
+                                  key_padding_mask=src_key_padding_mask)[0]
+        src_inst = self.norm1(src_inst + self.dropout1(src_inst2))
+
+        # src_obj = self.obj_proj(src_obj)
+        # q_o = k_o = self.with_pos_embed(src_obj, pos)
+        # src_obj2 = self.self_attn(q_o, k_o, value=src_obj, attn_mask=src_mask,
+        #                           key_padding_mask=src_key_padding_mask)[0]
+        # src_obj = self.norm1(src_obj + self.dropout1(src_obj2))
+
+        src_verb = self.verb_proj(src_verb)
+        q_v = k_v = self.with_pos_embed(src_verb, pos)
+        src_verb2 = self.self_attn(q_v, k_v, value=src_verb, attn_mask=src_mask,
+                                   key_padding_mask=src_key_padding_mask)[0]
+        src_verb = self.norm1(src_verb + self.dropout1(src_verb2))
+
+        src_inst, src_verb = self.forward_cross_attn(src_inst, src_verb, pos,
+                                                             src_key_padding_mask)
+        src_inst2 = self.linear2(self.dropout(self.activation(self.linear1(src_inst), inplace=True)))
+        src_inst = self.norm2(src_inst + self.dropout2(src_inst2))
+
+        # src_obj2 = self.linear2(self.dropout(self.activation(self.linear1(src_obj), inplace=True)))
+        # src_obj = self.norm2(src_obj + self.dropout2(src_obj2))
+
+        src_verb2 = self.linear2(self.dropout(self.activation(self.linear1(src_verb), inplace=True)))
+        src_verb = self.norm2(src_verb + self.dropout2(src_verb2))
+
+        return src_sub, src_obj, src_verb
+
+    def forward_pre(self, src_sub, src_obj, src_verb,
+                src_mask: Optional[Tensor] = None,
+                src_key_padding_mask: Optional[Tensor] = None,
+                pos: Optional[Tensor] = None):
+        src_sub = self.sub_proj(src_sub)
+        src_sub2 = self.norm_sub(src_sub)
+        q_s = k_s = self.with_pos_embed(src_sub2, pos)
+        src_sub2 = self.self_attn(q_s, k_s, value=src_sub2, attn_mask=src_mask,
+                                  key_padding_mask=src_key_padding_mask)[0]
+        src_sub = src_sub + self.dropout_sub(src_sub2)
+
+        src_obj = self.obj_proj(src_obj)
+        src_obj2 = self.norm_obj(src_obj)
+        q_o = k_o = self.with_pos_embed(src_obj2, pos)
+        src_obj2 = self.self_attn(q_o, k_o, value=src_obj2, attn_mask=src_mask,
+                                  key_padding_mask=src_key_padding_mask)[0]
+        src_obj = src_obj + self.dropout_sub(src_obj2)
+
+        src_verb = self.verb_proj(src_verb)
+        src_verb2 = self.norm_verb(src_verb)
+        q_v = k_v = self.with_pos_embed(src_verb2, pos)
+        src_verb2 = self.self_attn(q_v, k_v, value=src_verb2, attn_mask=src_mask,
+                                   key_padding_mask=src_key_padding_mask)[0]
+        src_verb = src_verb + self.dropout_verb(src_verb2)
+
+        src_sub, src_obj, src_verb = self.forward_cross_attn(src_sub, src_obj, src_verb, pos,
+                                                             src_key_padding_mask)
+        src_sub2 = self.norm2(src_sub)
+        src_sub2 = self.linear2(self.dropout(self.activation(self.linear1(src_sub2), inplace=True)))
+        src_sub = src_sub + self.dropout2(src_sub2)
+
+        src_obj2 = self.norm2(src_obj)
+        src_obj2 = self.linear2(self.dropout(self.activation(self.linear1(src_obj2), inplace=True)))
+        src_obj = src_obj + self.dropout2(src_obj2)
+
+        src_verb2 = self.norm2(src_verb)
+        src_verb2 = self.linear2(self.dropout(self.activation(self.linear1(src_verb2), inplace=True)))
+        src_verb = src_verb + self.dropout2(src_verb2)
+
+        return src_sub, src_obj, src_verb
+
+    def forward(self, src_inst, src_verb,
+                src_mask: Optional[Tensor] = None,
+                src_key_padding_mask: Optional[Tensor] = None,
+                pos: Optional[Tensor] = None):
+        if self.normalize_before:
+            raise NotImplementedError, "Use post mode"
+            return self.forward_pre(src_sub, src_obj, src_verb, src_mask,
+                                    src_key_padding_mask, pos)
+        return self.forward_post(src_inst, src_verb, src_mask,
+                                 src_key_padding_mask, pos)
 
 
 def _get_clones(module, N):
